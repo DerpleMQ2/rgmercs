8ac34937e78d265c68e10f4cfab445a20f4bb224
JoeBostic <joseph.bostic@gmail.com>
2024-04-29 05:23:57 -0700

    Moves long recast spell off of the bottom spell gem.
    This moves the mage's gather mana line of spells off of the bottom spell gem. Since the bottom spell gem is swapped out frequently, the gather mana spell was always in perpetual artificial cooldown. The fast cooldown pet heal spell is swapped into the bottom spell gem instead. You get the same functionality except that gather mana spell can now more often be able to be cast.

--- class_configs/mag_class_config.lua
+++ class_configs/mag_class_config.lua
@@ -1821,21 +1821,21 @@ _ClassConfig      = {
             gem = 8,
             cond = function(self, gem) return mq.TLO.Me.NumGems() >= gem end,
             spells = {
-                { name = "PetHealSpell", },
+                { name = "DichoSpell", },
             },
         },
         {
             gem = 9,
             cond = function(self, gem) return mq.TLO.Me.NumGems() >= gem end,
             spells = {
-                { name = "DichoSpell", },
+                { name = "TwinCast", },
             },
         },
         {
             gem = 10,
             cond = function(self, gem) return mq.TLO.Me.NumGems() >= gem end,
             spells = {
-                { name = "TwinCast", },
+                { name = "GatherMana", },
             },
         },
         {
@@ -1849,7 +1849,7 @@ _ClassConfig      = {
             gem = 12,
             cond = function(self, gem) return mq.TLO.Me.NumGems() >= gem end,
             spells = {
-                { name = "GatherMana", },
+                { name = "PetHealSpell", },
             },
         },
     },
