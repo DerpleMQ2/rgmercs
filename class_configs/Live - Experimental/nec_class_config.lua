6685c3cc5d6740b6194ac05c06b9fe7221e389d3
AlgarDude <43156286+AlgarDude@users.noreply.github.com>
2024-10-08 20:44:28 -0400

    Experimental config updates

--- class_configs/Live - Experimental/nec_class_config.lua
+++ class_configs/Live - Experimental/nec_class_config.lua
@@ -751,7 +751,7 @@ local _ClassConfig = {
             targetId = function(self) return { mq.TLO.Me.ID(), } end,
             cond = function(self, combat_state)
                 return combat_state == "Downtime" and
-                    RGMercUtils.DoBuffCheck() and RGMercConfig:GetTimeSinceLastMove() > RGMercUtils.GetSetting('BuffWaitMoveTimer')
+                    RGMercUtils.DoBuffCheck()
             end,
         },
         {
