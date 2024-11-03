009c6e6af4a1b71d8ce8c8dfc5b8de3cd9c36b66
Derple <derple@ntsj.com>
2024-11-02 20:37:15 -0700

    Pull Nav Dist message now matches the rest of the formating.

--- modules/pull.lua
+++ modules/pull.lua
@@ -1410,8 +1410,8 @@ function Module:GetPullableSpawns()
         end
 
         if not canPath or navDist > maxPathRange then
-            Logger.log_debug("\atPULL::FindTarget \aoPath check failed for spawn %s (%d) - dist(%d) canPath(%s)", spawn.CleanName(), spawn.ID(), navDist,
-                Strings.BoolToColorString(canPath))
+            Logger.log_debug("\atPULL::FindTarget \awFindTarget :: Spawn \am%s\aw (\at%d\aw) \aoPath check failed - dist(%d) canPath(%s)", spawn.CleanName(),
+                spawn.ID(), navDist, Strings.BoolToColorString(canPath))
             return false
         end
 
