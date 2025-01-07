e558cc590aa26a073def884fde8a7d28f5178bb6
AlgarDude <43156286+AlgarDude@users.noreply.github.com>
2025-01-07 17:49:23 -0500

    "Ranged" Pull Ability Fix (Thanks Daneki!) (#358)
    * Added "Ammo" to valid ranged weapon types for pull abilities.

--- modules/pull.lua
+++ modules/pull.lua
@@ -117,7 +117,7 @@ Module.Constants.PullAbilities         = {
         AbilityRange = function() return mq.TLO.Me.Inventory("ranged").Range() end,
         cond = function(self)
             local rangedType = (mq.TLO.Me.Inventory("ranged").Type() or ""):lower()
-            local rangedTypes = Set.new({ "archery", "bow", "throwingv1", "throwing", "throwingv2", })
+            local rangedTypes = Set.new({ "archery", "bow", "throwingv1", "throwing", "throwingv2", "ammo", })
             return rangedTypes:contains(rangedType)
         end,
     },
