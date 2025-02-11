7cc4fdc7f02eb1614442b0892151d1c04808bda3
AlgarDude <43156286+AlgarDude@users.noreply.github.com>
2025-02-10 19:46:14 -0500

    Equipment Update
    * Fixed an instance in which the "Do" settings for Charm or Chest could be saved as true if a PC on an emu build first loaded and adjusted settings with a Live config.

--- class_configs/Live - Experimental/dru_class_config.lua
+++ class_configs/Live - Experimental/dru_class_config.lua
@@ -1452,7 +1452,7 @@ local _ClassConfig = {
             DisplayName = "Do Chest Click",
             Category = "Utilities",
             Tooltip = "Click your chest item",
-            Default = true,
+            Default = not mq.TLO.MacroQuest.BuildName() == "Emu",
             FAQ = "Why am I not clicking my chest item?",
             Answer = "Make sure [DoChestClick] is enabled. If you are in Heal Mode, you may not be clicking your chest item.",
         },
