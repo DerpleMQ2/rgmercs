6528072b4b43efddb97200badac423399876f896
C.J. Manca <cjmanca@users.noreply.github.com>
2025-06-09 16:09:38 -0600

    Cure Update (#675)
    * Added support for server_pcname DanNet peer names in cure checks.

--- modules/class.lua
+++ modules/class.lua
@@ -1023,34 +1023,35 @@ function Module:RunCureRotation()
     }
 
     -- Me.TotalCounters does not work on emu we need to check everything.
-
     for i = 1, dannetPeers do
         ---@diagnostic disable-next-line: redundant-parameter
         local peer = mq.TLO.DanNet.Peers(i)()
         if peer and peer:len() > 0 then
-            if mq.TLO.SpawnCount(string.format("pc =%s radius 150", peer))() == 1 then
+            local startindex = string.find(peer, "_")
+            if startindex then
+                peer = string.sub(peer, startindex + 1)
+            end
+            local cureTarget = mq.TLO.Spawn(string.format("pc =%s", peer))
+
+            --current max range on live with raid gear is 137, radiant cure still limited to 100, but CureNow includes range checks
+            if cureTarget and cureTarget() and cureTarget.Distance() < 150 then
                 Logger.log_verbose("\ag[Cures] %s is in range - checking for curables", peer)
                 for _, data in ipairs(checks) do
                     local effectId = DanNet.query(peer, data.check, 1000) or "null"
                     Logger.log_verbose("\ay[Cures] %s :: %s [%s] => %s", peer, data.check, data.type, effectId)
 
                     if effectId:lower() ~= "null" and effectId ~= "0" then
-                        local cureTarget = mq.TLO.Spawn(string.format("pc =%s", peer))
-                        if cureTarget and cureTarget() then
-                            -- Cure it!
-                            if self.ClassConfig.Cures and self.ClassConfig.Cures.CureNow then
-                                Comms.HandleAnnounce(
-                                    string.format('Attempting to cure %s of %s', cureTarget.CleanName() or "Target", data.type),
-                                    Config:GetSetting('CureAnnounceGroup'),
-                                    Config:GetSetting('CureAnnounce'))
-                                Core.SafeCallFunc("CureNow", self.ClassConfig.Cures.CureNow, self, data.type, cureTarget.ID())
-                            end
+                        -- Cure it!
+                        if self.ClassConfig.Cures and self.ClassConfig.Cures.CureNow then
+                            Comms.HandleAnnounce(string.format('Attempting to cure %s of %s', cureTarget.CleanName() or "Target", data.type), Config:GetSetting('CureAnnounceGroup'),
+                                Config:GetSetting('CureAnnounce'))
+                            Core.SafeCallFunc("CureNow", self.ClassConfig.Cures.CureNow, self, data.type, cureTarget.ID())
                         end
                     end
                 end
+            else
+                Logger.log_verbose("\ao[Cures] %d::%s is in \arNOT\ao range", i, peer or "Unknown")
             end
-        else
-            Logger.log_verbose("\ao[Cures] %d::%s is in \arNOT\ao range", i, peer or "Unknown")
         end
     end
 end
