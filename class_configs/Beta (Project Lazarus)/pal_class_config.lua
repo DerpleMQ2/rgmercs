0c70c900694810a8ba49b827dbd757208fbc5b17
AlgarDude <43156286+AlgarDude@users.noreply.github.com>
2025-07-20 20:55:39 -0400

    Typo Correction

--- class_configs/Beta (Project Lazarus)/pal_class_config.lua
+++ class_configs/Beta (Project Lazarus)/pal_class_config.lua
@@ -457,7 +457,7 @@ return {
             end,
             targetId = function(self) return Targeting.CheckForAutoTargetID() end,
             cond = function(self, combat_state)
-                if (Config:GetSetting('AEStu') == 2 and Core.GetMainAssistPctHPs() > Config:GetSetting('EmergencyStart')) or Config:GetSetting('DoAEStun') == 1 then return false end
+                if (Config:GetSetting('AEStun') == 2 and Core.GetMainAssistPctHPs() > Config:GetSetting('EmergencyStart')) or Config:GetSetting('DoAEStun') == 1 then return false end
                 return combat_state == "Combat" and self.ClassConfig.HelperFunctions.AETargetCheck(true)
             end,
         },
