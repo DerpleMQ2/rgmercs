c83cd391b13b6b75c2ecf738ff21d88e5825a2ff
AlgarDude <43156286+AlgarDude@users.noreply.github.com>
2025-09-24 22:27:38 -0400

    Pet Heal Adjustment (#804)
    * Default class configs for healers (inc. PAL)  have been adjusted to no longer use "big heal" rotations on pets (if pet healing is enabled in the first place).
    * * Hybrid and pet class healing has not been adjusted.
    
    * Made minor adjustments to pet heal settings and tooltips for clarity.

--- class_configs/Project Lazarus/clr_class_config.lua
+++ class_configs/Project Lazarus/clr_class_config.lua
@@ -382,7 +382,7 @@ local _ClassConfig = {
             state = 1,
             steps = 1,
             cond = function(self, target)
-                return Targeting.BigHealsNeeded(target)
+                return Targeting.BigHealsNeeded(target) and not Targeting.TargetIsType("pet", target)
             end,
         },
         {
