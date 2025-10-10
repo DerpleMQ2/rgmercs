8fd499a2dc4b94f1eb91ace6b540af11079abd5e
Derple <derple@ntsj.com>
2025-10-09 19:43:52 -0700

    Fix for clickies rarely crashing if you smash the button realllly fast

--- init.lua
+++ init.lua
@@ -302,6 +302,7 @@ local function Main()
         end
         Modules:ExecModule("Drag", "GiveTime", curState)
         Modules:ExecModule("Debug", "GiveTime", curState)
+        Modules:ExecModule("Clickies", "ValidateClickies")
         Modules:ExecAll("WriteSettings") -- this needs to happen even when paused.
         return
     end
