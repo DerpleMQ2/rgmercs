ae4b6e4790a689c73b14950e9ec19ebaf6a03a03
AlgarDude <43156286+AlgarDude@users.noreply.github.com>
2025-10-12 20:31:44 -0400

    NEC(Laz)- Fix for lich spell spam (Thanks Redfrog!)

--- class_configs/Project Lazarus/nec_class_config.lua
+++ class_configs/Project Lazarus/nec_class_config.lua
@@ -528,7 +528,7 @@ local _ClassConfig = {
                 type = "Spell",
                 load_cond = function(self) return Config:GetSetting('DoLich') end,
                 cond = function(self, spell)
-                    return mq.TLO.Me.PctHPs() > Config:GetSetting('StopLichHP') and mq.TLO.Me.PctMana() < Config:GetSetting('StartLichMana')
+                    return Casting.SelfBuffCheck(spell) and mq.TLO.Me.PctHPs() > Config:GetSetting('StopLichHP') and mq.TLO.Me.PctMana() < Config:GetSetting('StartLichMana')
                 end,
             },
             {
@@ -810,7 +810,7 @@ local _ClassConfig = {
                 load_cond = function(self) return Config:GetSetting('DoLich') end,
                 active_cond = function(self, spell) return Casting.IHaveBuff(spell) end,
                 cond = function(self, spell)
-                    return mq.TLO.Me.PctHPs() > Config:GetSetting('StopLichHP') and mq.TLO.Me.PctMana() < Config:GetSetting('StartLichMana')
+                    return Casting.SelfBuffCheck(spell) and mq.TLO.Me.PctHPs() > Config:GetSetting('StopLichHP') and mq.TLO.Me.PctMana() < Config:GetSetting('StartLichMana')
                 end,
             },
             {
